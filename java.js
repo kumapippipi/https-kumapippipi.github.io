d049e4b58015d5efe34fd92289ed37798496bbc2
kumapu <93045352+kumapippipi@users.noreply.github.com>
2023-02-21 11:31:02 +0900

    Create java.js

---
+++ java.js
@@ -0,0 +1,21 @@
+const choices = document.querySelectorAll('.choices button');
+const result = document.getElementById('result');
+
+choices.forEach((choice) => {
+    choice.addEventListener('click', () => {
+        if (choice.id === 'choice-1' || choice.id === 'choice-2' || choice.id === 'choice-3' || choice.id === 'choice-5' || choice.id === 'choice-6' || choice.id === 'choice-7') {
+            choice.classList.add('correct');
+            result.textContent = '正解！';
+            '理由は'
+        } else {
+            choice.classList.add('incorrect');
+            result.textContent = '不正解！';
+        }
+        choices.forEach((c) => {
+            if (c.id !== choice.id) {
+                c.classList.add('incorrect');
+            }
+        });
+    });
+});
+
